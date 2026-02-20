9d342b26f016eb63822e7a4eacec5f3f55e920ef
siriparams <siriparamathma06@gmail.com>
2026-02-20 13:28:34 +0530

    fix: allow render host in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
-      allowedHosts: ['eosinlike-semiseriously-jamal.ngrok-free.dev'],
+      allowedHosts: ['*'],
     },
     plugins: [react()],
     define: {
